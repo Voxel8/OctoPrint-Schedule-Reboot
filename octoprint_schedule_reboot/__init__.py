32dacef9a9e8cd3deec42fc843eed1aa0dfe242e
Kevin Murphy <murphyk3@wit.edu>
2016-04-13 14:24:13 -0400

    Rename variables for clarification; Pep8

--- octoprint_schedule_reboot/__init__.py
+++ octoprint_schedule_reboot/__init__.py
@@ -1,22 +1,19 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import os
 from time import sleep
 from threading import Thread
-import os
 import octoprint.plugin
-import flask
 from octoprint.events import eventManager, Events
 
-Events.EVENT_MESSAGE = "ScheduleReboot"
+Events.SCHEDULE_REBOOT_EVENT = "ScheduleReboot"
 
 
 class Schedule_rebootPlugin(octoprint.plugin.SimpleApiPlugin):
 
     def __init__(self):
         self.remaining = None
-
-    # ~~ SimpleApiPlugin mixin
 
     def get_api_commands(self):
         return dict(
@@ -27,29 +24,32 @@ class Schedule_rebootPlugin(octoprint.plugin.SimpleApiPlugin):
         )
 
     def on_api_command(self, command, data):
+        log_msg = None
         if command == "schedule_reboot":
             self._logger.info("Scheduling reboot...")
             if not self.printer_is_printing():
-                self._logger.info("Printer not in use, rebooting in 60 seconds")
+                log_msg = "Printer not in use, rebooting in 60 seconds"
+                self._logger.info(log_msg)
                 self.initiate_reboot(60)
             else:
-                self._logger.info("Printer in use, will try again in 60 minutes")
+                log_msg = "Printer in use, will try again in 60 minutes"
+                self._logger.info(log_msg)
                 self.schedule_reboot(7200)
 
         elif command == "cancel":
-            self._logger.info("Scheduled reboot cancelled, will try again in 60 minutes")
+            log_msg = "Scheduled reboot cancelled, trying again in 60 minutes"
+            self._logger.info(log_msg)
             self._cancel_reboot = True
             self.schedule_reboot(7200)
 
         elif command == "reboot_now":
-            self._logger.info("Reboot called immediately.")
+            log_msg = "Reboot called immediately"
+            self._logger.info(log_msg)
             os.system('sudo reboot')
 
         elif command == "trigger_status":
             payload = {'duration': self.remaining}
-            eventManager().fire(Events.EVENT_MESSAGE, payload)
-
-    #########################
+            eventManager().fire(Events.SCHEDULE_REBOOT_EVENT, payload)
 
     def printer_is_printing(self):
         return self._printer.is_printing() or self._printer.is_paused()
@@ -62,7 +62,7 @@ class Schedule_rebootPlugin(octoprint.plugin.SimpleApiPlugin):
                                      args=(secs_from_now,))
         self._reboot_thread.start()
         payload = {'duration': secs_from_now}
-        eventManager().fire(Events.EVENT_MESSAGE, payload)
+        eventManager().fire(Events.SCHEDULE_REBOOT_EVENT, payload)
 
     def schedule_reboot(self, secs_from_now):
         """ Initiate a reboot in the future,
@@ -86,31 +86,6 @@ class Schedule_rebootPlugin(octoprint.plugin.SimpleApiPlugin):
         sleep(secs_from_now)
         self.initiate_reboot(60)
 
-    # ~~ Softwareupdate hook
-
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-        # for details.
-        return dict(
-            schedule_reboot=dict(
-                displayName="Schedule Reboot",
-                displayVersion=self._plugin_version,
-
-                # version check: github repository
-                type="github_commit",
-                user="voxel8",
-                repo="OctoPrint-Schedule-Reboot",
-                # current=self._plugin_version,
-
-                # update method: pip
-                pip="https://github.com/voxel8/OctoPrint-Schedule-Reboot/archive/{target_version}.zip"
-            )
-        )
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Schedule Reboot"
 
 
@@ -119,6 +94,7 @@ def __plugin_load__():
     __plugin_implementation__ = Schedule_rebootPlugin()
 
     global __plugin_hooks__
+    update_hook = "octoprint.plugin.softwareupdate.check_config"
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+        update_hook: __plugin_implementation__.get_update_information
     }
