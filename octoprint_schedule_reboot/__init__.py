fab8488667b2d5ad84c2705e30d911837957399d
Kevin Murphy <murphyk3@wit.edu>
2016-04-13 10:12:37 -0400

    Send events to OctoPrint; add reboot_now command

--- octoprint_schedule_reboot/__init__.py
+++ octoprint_schedule_reboot/__init__.py
@@ -4,64 +4,28 @@ from __future__ import absolute_import
 from time import sleep
 from threading import Thread
 import os
-
 import octoprint.plugin
+import flask
+from octoprint.events import eventManager, Events
 
-class Schedule_rebootPlugin(octoprint.plugin.SettingsPlugin,
-                            octoprint.plugin.AssetPlugin,
-                            octoprint.plugin.SimpleApiPlugin,
-                            octoprint.plugin.TemplatePlugin):
+Events.EVENT_MESSAGE = "ScheduleReboot"
 
-    ##~~ SettingsPlugin mixin
 
-    def get_settings_defaults(self):
-        return dict(
-            # put your plugin's default settings here
-        )
+class Schedule_rebootPlugin(octoprint.plugin.SimpleApiPlugin):
 
-    ##~~ AssetPlugin mixin
+    def __init__(self):
+        self.remaining = None
 
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return dict(
-            #js=["js/schedule_reboot.js"],
-            #css=["css/schedule_reboot.css"],
-            #less=["less/schedule_reboot.less"]
-        )
-
-    ##~~ Softwareupdate hook
-
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-        # for details.
-        return dict(
-            schedule_reboot=dict(
-                displayName="Schedule Reboot",
-                displayVersion=self._plugin_version,
-
-                # version check: github repository
-                type="github_commit",
-                user="voxel8",
-                repo="OctoPrint-Schedule-Reboot",
-                #current=self._plugin_version,
-
-                # update method: pip
-                pip="https://github.com/voxel8/OctoPrint-Schedule-Reboot/archive/{target_version}.zip"
-            )
-        )
-
-    ##~~ SimpleApiPlugin mixin
+    # ~~ SimpleApiPlugin mixin
 
     def get_api_commands(self):
         return dict(
             schedule_reboot=[],
             cancel=[],
+            reboot_now=[],
         )
 
     def on_api_command(self, command, data):
-
         if command == "schedule_reboot":
             self._logger.info("Scheduling reboot...")
             if not self.printer_is_printing():
@@ -76,6 +40,15 @@ class Schedule_rebootPlugin(octoprint.plugin.SettingsPlugin,
             self._cancel_reboot = True
             self.schedule_reboot(7200)
 
+        elif command == "reboot_now":
+            self._logger.info("Reboot called immediately.")
+            os.system('sudo reboot -f')
+
+    def on_api_get(self, request):
+        payload = {'duration': self.remaining}
+        eventManager().fire(Events.EVENT_MESSAGE, payload)
+        return flask.jsonify(remaining=self.remaining)
+
     #########################
 
     def printer_is_printing(self):
@@ -88,6 +61,8 @@ class Schedule_rebootPlugin(octoprint.plugin.SettingsPlugin,
         self._reboot_thread = Thread(target=self._reboot_worker,
                                      args=(secs_from_now,))
         self._reboot_thread.start()
+        payload = {'duration': secs_from_now}
+        eventManager().fire(Events.EVENT_MESSAGE, payload)
 
     def schedule_reboot(self, secs_from_now):
         """ Initiate a reboot in the future,
@@ -97,24 +72,47 @@ class Schedule_rebootPlugin(octoprint.plugin.SettingsPlugin,
         self._future_reboot_thread.start()
 
     def _reboot_worker(self, secs_from_now):
-        remaining = secs_from_now
-        while (not self._cancel_reboot) and remaining > 0:
-            remaining -= 1
+        self.remaining = secs_from_now
+        while (not self._cancel_reboot) and self.remaining > 0:
+            self.remaining -= 1
             sleep(1)
         if not self._cancel_reboot:
-            os.system('sudo reboot now')
+            os.system('sudo reboot -f')
         else:
+            self.remaining = None
             self._logger.info("Reboot thread aborted")
 
     def _future_reboot(self, secs_from_now):
         sleep(secs_from_now)
         self.initiate_reboot(60)
 
+    # ~~ Softwareupdate hook
+
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            schedule_reboot=dict(
+                displayName="Schedule Reboot",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_commit",
+                user="voxel8",
+                repo="OctoPrint-Schedule-Reboot",
+                # current=self._plugin_version,
+
+                # update method: pip
+                pip="https://github.com/voxel8/OctoPrint-Schedule-Reboot/archive/{target_version}.zip"
+            )
+        )
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Schedule Reboot"
+
 
 def __plugin_load__():
     global __plugin_implementation__
@@ -124,4 +122,3 @@ def __plugin_load__():
     __plugin_hooks__ = {
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
-
